376887119ad3d34d9063c4d5ced5a6330d1c0966
Henry Wang <henrywangsihhung100@gmail.com>
2021-10-18 23:14:32 +0800

    added typed.js package

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import Typed from 'typed.js';
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
-  styleUrls: ['./intro.component.css']
+  styleUrls: ['./intro.component.css'],
 })
 export class IntroComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    const options = {
+      strings: ['Innovation.', 'Discovery.'],
+      typeSpeed: 100,
+      backSpeed: 100,
+      showCursor: true,
+      cursorChar: '|',
+      loop: true,
+    };
 
+    const typed = new Typed('.typed-element', options);
+  }
 }
